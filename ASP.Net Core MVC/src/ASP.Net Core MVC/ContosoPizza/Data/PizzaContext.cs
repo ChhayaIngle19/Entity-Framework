e7e2caf1145b7784db1e7161640f536ed289e79f
ChhayaIngle19 <chhaya@entujn.com>
2025-02-17 22:19:04 +0530

    Web UI project ContosoPizza

---
+++ ASP.Net Core MVC/src/ASP.Net Core MVC/ContosoPizza/Data/PizzaContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ContosoPizza.Data
+{
+    public class PizzaContext : DbContext
+    {
+        // Add a constructor that accepts DbContextOptions
+        public PizzaContext(DbContextOptions<PizzaContext> options) : base(options)
+        {
+        }
+        public DbSet<ContosoPizza.Models.Pizza>? Pizzas { get; set; }
+
+    }
+}
---
+++ ASP.Net Core MVC/src/ASP.Net Core MVC/ContosoPizza/Models/Pizza.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoPizza.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+        public PizzaSize Size { get; set; }
+        public bool IsGlutenFree { get; set; }
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Price { get; set; }
+    }
+
+    public enum PizzaSize { Small, Medium, Large }
+}
